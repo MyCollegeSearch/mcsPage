3b411cdcbb6a0afd9acedac1c06060c475781d89
Ryan Cross <studiopod.ryancross@outlook.com>
2015-04-18 15:23:09 -0500

    "added secure folder & database.php file for db connection"

---
+++ secure/database.php
@@ -0,0 +1,7 @@
+<?php
+	DEFINE ("HOST", "host=dbhost-pgsql.cs.missouri.edu");
+	DEFINE("DBNAME","dbname=cs3380s15grp19");
+	DEFINE("USERNAME", "user=cs3380s15grp19");
+	DEFINE("PASSWORD", "password=iw2Gtb2E");
+?>
+
